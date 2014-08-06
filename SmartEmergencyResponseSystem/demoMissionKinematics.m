05b8693d866ea240668418111d0a1aba495ceed9
Justyna Zander <justyna.zander@gmail.com>
2014-08-06 11:30:28 -0400

    Update demoMissionKinematics.m

--- SmartEmergencyResponseSystem/demoMissionKinematics.m
+++ SmartEmergencyResponseSystem/demoMissionKinematics.m
@@ -1,3 +1,6 @@
+% ******************************************************************************
+% Copyright (c) 2014, The MathWorks, Inc. All rights reserved.
+
 function demoMissionKinematics
 %========================== Set up mission ================================     
     %imData = imread('SFHiRes.png');

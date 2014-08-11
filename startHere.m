6b2c421976d983b19610d89150de9ffa59de094e
Justyna Zander <justyna.zander@gmail.com>
2014-08-11 15:00:47 -0400

    Addding news

---
+++ startHere.m
@@ -0,0 +1,49 @@
+% ******************************************************************************
+% Copyright (c) 2014, The MathWorks, Inc. All rights reserved.
+% ******************************************************************************
+%  
+% How to run SERS in a loop mode. 
+% *******************************
+% 
+% 
+% Open 4 MATLAB sessions (matlab.exe -hgVersion 2). 
+% In each of the MATLAB sessions: 
+% - Command window: addpath(genpath(pwd))
+%   
+%% MATLAB session 1: 
+% Command window: y = QuadrotorOptimizationReceiptAdaptor('127.0.0.1',10020,10030)
+%  
+%% MATLAB session 2 (32 bit):
+% Command window: GE = GeManager
+% If you have no 32 bit MATLAB version, neglect this step. The Simulink to Google Earth
+% interface is not going to run, though. 
+%
+%% MATLAB session 3:
+% Command window: 
+% startHere
+%  
+%% MATLAB session 4: 
+% Command window: fakeShAir
+
+%% SmartEmergencyResponseSystem
+addpath(genpath([pwd '\SmartEmergencyResponseSystem']));
+
+%% Initiate user specified workflow
+    newDemoMission    = 'Demonstrate the mission kinematics and dynamics';
+    newDemoMissionK   = 'Demonstrate the mission kinematics';
+    newRunMission     = 'Run and setup the mission yourself';
+    
+        workflow = questdlg( 'Make sure to first run two other apps in separate MATLAB sessions: (1) SERS server, and (2) SERS requests. Please choose the mode of the rescue mission.','Mission workflow',...
+        newDemoMission, newDemoMissionK, newRunMission, newDemoMissionK);
+    
+    switch workflow
+        case newDemoMission
+            clear newDemoMission newDemoMissionK newRunMission workflow
+            demoMission
+        case newDemoMissionK
+            clear newDemoMission newDemoMissionK newRunMission workflow
+            demoMissionKinematics
+        case newRunMission
+            clear newDemoMission newDemoMissionK newRunMission workflow
+            runMission
+    end

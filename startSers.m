3d32618b4a411ebab0ab2987164168558c7289e5
Justyna Zander <justyna.zander@gmail.com>
2014-08-11 15:02:13 -0400

    add

---
+++ startSers.m
@@ -0,0 +1,14 @@
+% ******************************************************************************
+% Copyright (c) 2014, The MathWorks, Inc. All rights reserved.
+% ******************************************************************************
+% startSers() is used to build the MATLAB app
+
+function y = startSers()
+%STARTSERS Summary of this function goes here
+%   Detailed explanation goes here
+
+startHere 
+disp('Starting the SERS Project ');
+y = 'Congratulations! You successfully started the SERS application.';
+
+
